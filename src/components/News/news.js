ed078896079bf090b1d40d73d6e46aaca3bcfc58
ibnay <zainishaaah@gmail.com>
2025-09-04 15:16:10 +0500

    create spinner component

--- src/components/News/news.js
+++ src/components/News/news.js
@@ -12,20 +12,24 @@ export default class news extends Component {
             loading: false,
             page: 1
         }
+        this.pageSize = props.pageSize;
     }
 
     fetchArticles = async (page) => {
-        // const Url = `https://newsapi.org/v2/everything?q=tesla&from=2025-08-02&sortBy=publishedAt&page=${page}&pageSize=${pageSize}&apiKey=70d0b7f4bc7c49d6927b3b7595385f6b`;
+        // const Url = `https://newsapi.org/v2/everything?q=tesla&from=2025-08-02&sortBy=publishedAt&page=${page}&this.pageSize=${this.pageSize}&apiKey=70d0b7f4bc7c49d6927b3b7595385f6b`;
+        this.setState({ loading: true });
         // const data = await fetch(Url);
         // const parseData = await data.json();
-        const pageSize = 4;
-        const start = (page - 1) * pageSize;
-        const end = page * pageSize;
+        const start = (page - 1) * this.pageSize;
+        const end = page * this.pageSize;
     
-        this.setState({
-            articles: this.articles.slice(start, end),
-            page: page,
-        });
+        setTimeout(() => {
+            this.setState({
+                articles: articlesData.slice(start, end),
+                page: page,
+                loading: false
+            });   
+        },500);
     
     }; 
 
@@ -40,7 +44,7 @@ export default class news extends Component {
     };
       
     handleNextClick = async () => {
-        const maxPages = Math.ceil(16 / 4);
+        const maxPages = Math.ceil(16 / this.pageSize);
         if (this.state.page < maxPages) {
           await this.fetchArticles(this.state.page + 1);      
         }
@@ -49,10 +53,11 @@ export default class news extends Component {
     render() {
         return (
             <>
-                <h2 className='container'>Top Headlines</h2>
+                <h2 className='container text-center'>Top Headlines</h2>
+                {this.state.loading && <Spinner />}
                 <div className="container">
                     <div className="row">
-                        {this.state.articles.map((element) => {
+                        {!this.state.loading && this.state.articles.map((element) => {
                            return <div key={element.url} className="col-12 col-md-6 col-lg-4 p-0 m-0">
                                 <NewsItems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
                             </div>
@@ -61,7 +66,7 @@ export default class news extends Component {
                     <div className="container d-flex justify-content-between p-0 m-0 mt-4 mb-4">
                         <button disabled={this.state.page<=1} className="btn btn-md btn-primary" onClick={this.handlePreviousClick}>&larr; Previous</button>
                         <span className="text-center mt-3">Page {this.state.page}</span>
-                        <button disabled={this.state.page >= Math.ceil(16 / 4) || this.state.articles.length < 4} className="btn btn-md btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
+                        <button disabled={this.state.page >= Math.ceil(16 / this.pageSize) || this.state.articles.length < this.pageSize} className="btn btn-md btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
                     </div>
                 </div>
             </>
